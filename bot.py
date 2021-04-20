3a4803b418b4c790bddafebd7605f737551d8f73
igor <i.selickiy@ya.ru>
2021-04-20 16:41:40 +0300

    Bot v1.2

--- bot.py
+++ bot.py
@@ -1,58 +1,49 @@
-import telebot
-from telebot import types
-import random
+from telebot import types, TeleBot
+from random import randint
 import pathlib
 import os
 
 work_dir = pathlib.Path(__file__).parent.absolute()
-bot = telebot.TeleBot('TOKEN')
+bot = TeleBot('TOKEN')
 
-
-def hello_audio(message):
-	hello_files = [
-		'music/Hello/privetkigovnoed.ogg',
+hello = [
+    	'music/Hello/privetkigovnoed.ogg',
 		'music/Hello/zdarovachertila.ogg',
 		'music/Hello/hulipripersya.ogg',
-	]
-	hello_len = len(hello_files)
-	hello_file = open(os.path.join(work_dir, hello_files[random.randint(0, hello_len) - 1]), 'rb')
-	bot.send_audio(message.chat.id, hello_file)
-	hello_file.close()
+    ]
 
-
-def history_audio(message):
-	history_files = [
-		'music/History/idinahui.ogg',
+history = [
+    	'music/History/idinahui.ogg',
 		'music/History/istioriahuynya.ogg',
 		'music/History/nihuyaneponyatno.ogg',
 		'music/History/prospis.ogg',
 		'music/History/putinpomozhet.ogg',
-	]
-	history_len = len(history_files)
-	history_file = open(os.path.join(work_dir, history_files[random.randint(0, history_len) - 1]), 'rb')
-	bot.send_audio(message.chat.id, history_file)
-	history_file.close()
+    ]
 
 
-def open_audio(message, idx):
-	audio_files = [
-		'music/manifest.mp3',
-	]
-	audio_file = open(os.path.join(work_dir, audio_files[idx]), 'rb')
-	bot.send_audio(message.chat.id, audio_file)
-	audio_file.close()
+def hello_audio(message,hello=hello):
+	with open(os.path.join(work_dir, hello[randint(0, len(hello)) - 1]), 'rb') as f:
+	    bot.send_audio(message.chat.id, f)
+
+
+def history_audio(message, history=history):
+	with open(os.path.join(work_dir, history[randint(0, len(history)) - 1]), 'rb') as f:
+	    bot.send_audio(message.chat.id, f)
+
+
+def manifest_audio(message):
+	with open(os.path.join(work_dir, 'music/manifest.mp3'), 'rb') as f:
+	    bot.send_audio(message.chat.id, f)
 
 
 def open_video(message):
-	video_file = open(os.path.join(work_dir, 'video/TUTORIAL.MP4'), 'rb')
-	bot.send_video(message.chat.id, video_file)
-	video_file.close()
+    with open(os.path.join(work_dir, 'video/TUTORIAL.MP4'), 'rb') as f:
+        bot.send_video(message.chat.id, f)
 
 
 def open_hat(message):
-	img_file = open(os.path.join(work_dir, 'img/chort_hat.png'), 'rb')
-	bot.send_document(message.chat.id, img_file)
-	img_file.close()
+	with open(os.path.join(work_dir, 'img/chort_hat.png'), 'rb') as f:
+	    bot.send_document(message.chat.id, f)
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -82,7 +73,7 @@ def echo_all(message):
 • Когда Молчание разрушает общество, мы делаем «Бип».\n\n • Образцовый участник не забывает одну минуту в сутки прощать Владимира Владимировича Путина. В 1:11 часов ночи по Москве.\n\n \
 • Покидая комнату, надо прощаться.\n\n • Все, что говорит чорт - есть истина! Извиняться нельзя.')
 	elif message.text == 'Манифест':
-		open_audio(message, 0)
+		manifest_audio(message)
 	elif message.text == 'Рассказать историю':
 		bot.send_message(message.chat.id, 'Напиши свою чертовски интересную историю, чтобы получить самый полезный совет')
 	else:
